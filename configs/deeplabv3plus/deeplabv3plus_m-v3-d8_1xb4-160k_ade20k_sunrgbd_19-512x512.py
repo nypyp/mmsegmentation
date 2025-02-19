f727ad4399a9d57cd2b02fb1751e3290327ec639
nypyp <nypypbro@gmail.com>
2025-02-19 23:49:06 +0800

    refactor(deeplabv3plus): 重构 DeepLabV3+ 配置文件
    - 更新配置文件名称，增加模型和数据集的详细信息
    - 修改模型架构，使用 MobileNetV3 作为 backbone
    - 更新数据集设置，使用 ADE20K 和 SUNRGBD 数据集
    - 调整训练参数，包括批量大小、学习率等
    - 添加新的 SegNext 模型配置文件

--- configs/deeplabv3plus/deeplabv3plus_m-v3-d8_1xb4-160k_ade20k_sunrgbd_19-512x512.py
+++ configs/deeplabv3plus/deeplabv3plus_m-v3-d8_1xb4-160k_ade20k_sunrgbd_19-512x512.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_base_/models/deeplabv3plus_r50-d8.py', '../_base_/datasets/ade20k_sunrgbd.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+    '../_base_/models/deeplabv3plus_r50-d8.py', '../_base_/datasets/ade20k_sunrgbd_19.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 crop_size = (512, 512)
@@ -12,33 +12,47 @@ model = dict(
         _delete_=True,
         type='MobileNetV3',
         arch='large',
-        out_indices=(0, 3, 16),
+        out_indices=(3, 7, 16),
         norm_cfg=norm_cfg),
     decode_head=dict(
-        _delete_=True,
-        type='AIFIHead',
+        type='DepthwiseSeparableASPPHead',
         in_channels=960,
+        in_index=2,
         channels=480,
+        dilations=(1, 12, 24, 36),
         c1_in_channels=24,
         c1_channels=48,
-        in_index=2,
         dropout_ratio=0.1,
-        num_classes=58,
+        num_classes=19,
         norm_cfg=norm_cfg,
         align_corners=False,
         loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
-    ),
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    # decode_head=dict(
+    #     _delete_=True,
+    #     type='AIFIHead',
+    #     in_channels=960,
+    #     channels=480,
+    #     c1_in_channels=24,
+    #     c1_channels=48,
+    #     in_index=2,
+    #     dropout_ratio=0.1,
+    #     num_classes=58,
+    #     norm_cfg=norm_cfg,
+    #     align_corners=False,
+    #     loss_decode=dict(
+    #         type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+    # ),
     auxiliary_head=dict(
         _delete_=True,
         type='FCNHead',
         in_channels=24,
-        in_index=1,
+        in_index=0,
         channels=256,
         num_convs=1,
         concat_input=False,
         dropout_ratio=0.1,
-        num_classes=58,
+        num_classes=19,
         norm_cfg=norm_cfg,
         align_corners=False,
         loss_decode=dict(
@@ -46,14 +60,14 @@ model = dict(
 )
 # dataset settings
 train_dataloader = dict(
-    batch_size=16,
+    batch_size=4,
     )
 
-visualizer = dict(
-    vis_backends=[dict(type='LocalVisBackend'),
-                dict(type='TensorboardVisBackend'),
-                dict(type='WandbVisBackend')]
-)
+# visualizer = dict(
+#     vis_backends=[dict(type='LocalVisBackend'),
+#                 dict(type='TensorboardVisBackend'),
+#                 dict(type='WandbVisBackend')]
+# )
 
 default_hooks = dict(
     visualization=dict(type='SegVisualizationHook',draw=True,interval=50)
